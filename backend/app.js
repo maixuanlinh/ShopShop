0a038a2737597df7e01531824fec04fe29b8b71d
maixuanlinh <maixuanlinhdkh@gmail.com>
2023-08-13 03:08:02 +0300

    modify App.js to serve the right static files

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,53 @@
 const express = require("express");
+const path = require("path");
 const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "https://shopshop.azurewebsites.net/",
+    origin: "https://shopshop.azurewebsites.net", // Removed trailing slash
     credentials: true,
   })
 );
-app.use("/", express.static("uploads"));
-app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}));
 
-//config
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// Use dotenv only in non-production environments
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({
-        path:"./config/.env"
-})
+  require("dotenv").config({
+    path: "./config/.env",
+  });
 }
 
-//import routes
+// If in production, serve frontend's build folder
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+// Import routes
 const user = require("./controller/user");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
 const event = require("./controller/event");
 const coupon = require("./controller/couponcode");
+
 app.use("/api/v2/user", user);
 app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
-// for error handling
+
+// For error handling
 app.use(ErrorHandler);
 
 module.exports = app;
